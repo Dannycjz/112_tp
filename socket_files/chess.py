b0cc79ea40ea3bec1693e186d06b9cdd20f820f9
Dannycjz <dannycjz@outlook.com>
2021-11-16 23:21:18 -0500

    implemented all movement features except for king

--- socket_files/chess.py
+++ socket_files/chess.py
@@ -59,28 +59,33 @@ def init_pieces():
 def mousePressed(app, event):
     x=event.x
     y=event.y
-    cell=selectCell(app, x, y)
-    if cell!=None:
-        row=cell[0]
-        col=cell[1]
-        if app.makingMove is False:
-            selectPiece(app, row, col)
-            updateValidMoves(app)
+    (row, col)=selectCell(app, x, y)
+    if app.makingMove is False:
+        app.outlined[row][col]=True
+        selectPiece(app, row, col)
+        updateValidMoves(app)
+    else:
+        if isValidMove(app, row, col):
+            makeMove(app, row, col)
+            clearOutlines(app)
         else:
-            if isValidMove(app, row, col):
-                makeMove(app, row, col)
-                clearOutlines(app)
+            unselectPiece(app)
+            clearOutlines(app)
 
 # Selects a cell on the chessboard
 def selectCell(app, x, y):
-    if app.makingMove is False:
-        for row in range(8):
-            for col in range(8):
-                (x0, y0, x1, y1) = getCellBounds(app, row, col)
-                if (x in range(int(x0), int(x1))) and (y in range(int(y0), int(y1))):
-                    app.outlined[row][col]=True
-                    selectedCell=(row, col)
-        return selectedCell
+    for row in range(8):
+        for col in range(8):
+            (x0, y0, x1, y1) = getCellBounds(app, row, col)
+            if (x in range(int(x0), int(x1))) and (y in range(int(y0), int(y1))):
+                return (row, col)
+
+# Unselect the selected piece
+def unselectPiece(app):
+    app.selectedPiece=None
+    app.currLoc=None
+    app.selectedPiece=None
+    app.makingMove=False
 
 # Selects a chess piece if the user clicks on one
 def selectPiece(app, row, col):
@@ -88,8 +93,6 @@ def selectPiece(app, row, col):
         app.selectedPiece=app.pieces[row][col]
         app.currLoc=(row, col)
         app.makingMove=True
-    else:
-        pass
 
 # Moves the selected chess piece to the destination cell
 def makeMove(app, row, col):
@@ -118,35 +121,207 @@ def updateValidMoves(app):
 def isValidMove(app, row, col):
     currR=app.currLoc[0]
     currC=app.currLoc[1]
+    color=app.selectedPiece[0]
     piece=app.selectedPiece[1]
+    # Checks for friendly piece collision
+    if app.pieces[row][col][0]==color: return False
+    # Checks valid moves based on piece type
     if piece=="pawn":
-        return isValidPawnMove(app, currR, currC, row, col)
+        return isValidPawnMove(app, currR, currC, row, col, color)
+    elif piece=="castle":
+        return isValidCastleMove(app, currR, currC, row, col)
+    elif piece=="knight":
+        return isValidKnightMove(currR, currC, row, col)
+    elif piece=="bishop":
+        return isValidBishopMove(app, currR, currC, row, col)
+    elif piece=="queen":
+        return (isValidBishopMove(app, currR, currC, row, col) or
+                isValidCastleMove(app, currR, currC, row, col))
 
 # Checks if [row][col] is a valid pawn move from [currR][currC]
-def isValidPawnMove(app, currR, currC, row, col):
-    color=app.selectedPiece[0]
+def isValidPawnMove(app, currR, currC, row, col, color):
     if color=="black":
+        # Empty cells move set
         if app.pieces[row][col]==("empty", "empty"):
             if (col==currC) and (currR==row-1):
                 return True
-            else:
-                return False
+            else: return False
+        # Enemy eating movement set
         else:
             if (currR==row-1) and ((col==currC-1) or (col==currC+1)):
                 return True
-            else:
-                return False
+            else: return False
     else:
+        # Empty cells move set
         if app.pieces[row][col]==("empty", "empty"):
             if (col==currC) and (currR==row+1):
                 return True
-            else:
-                return False
+            else: return False
+        # Enemy eating movement set
         else:
             if (currR==row+1) and ((col==currC-1) or (col==currC+1)):
                 return True
-            else:
-                return False
+            else: return False
+
+# Checks if [row][col] is a valid castle move from [currR][currC]
+def isValidCastleMove(app, currR, currC, row, col):
+    # Gets the furthest possible column positions
+    leftC, rightC=nearestPieceOnRow(app, currR, currC)
+    # Gets the furthest possible row positions
+    topR, botR=nearestPieceOnCol(app, currR, currC)
+    if col==currC:
+        if botR<=row<=topR:
+            return True
+        else: return False 
+    elif row==currR:
+        if leftC<=col<=rightC:
+            return True
+        else: return False 
+    else: return False
+ 
+ # Checks if [row][col] is a valid knight move from [currR][currC]
+def isValidKnightMove(currR, currC, row, col):
+    if (currR+1==row) or (currR-1==row):
+        if (currC+2==col) or (currC-2==col):
+            return True
+    elif (currC+1==col) or (currC-1==col):
+        if (currR+2==row) or (currR-2==row):
+            return True
+    return False
+
+# Checks if [row][col] is a valid bishop move from [currR][currC]
+def isValidBishopMove(app, currR, currC, row, col):
+    leftD, Lindex=Ldiag(currR, currC)
+    Lleft, Lright=nearestPieceLDiag(app, currR, currC)
+    rightD, Rindex=Rdiag(currR, currC)
+    Rleft, Rright=nearestPieceRDiag(app, currR, currC)
+    for coord in rightD[Rleft:Rright+1]:
+        if (row, col)==coord:
+            return True
+    for coord in leftD[Lleft:Lright+1]:
+        if (row, col)==coord:
+            return True
+    return False
+
+# Returns the coordinates of the closest pieces to [currR][currC] 
+# on the same left diagonal
+def nearestPieceLDiag(app, currR, currC):
+    diag, index=Ldiag(currR, currC)
+    left=0
+    right=len(diag)-1
+    i=index-1
+    while i>=0:
+        (row, col)=diag[i]
+        if app.pieces[row][col]!=("empty", "empty"):
+            left=i
+            break
+        i-=1
+    i=index+1
+    while i<len(diag):
+        (row, col)=diag[i]
+        if app.pieces[row][col]!=("empty", "empty"):
+            right=i
+            break
+        i+=1
+    return left, right
+
+# Returns the coordinates of the closest pieces to [currR][currC] 
+# on the same right diagonal
+def nearestPieceRDiag(app, currR, currC):
+    diag, index=Rdiag(currR, currC)
+    left=0
+    right=len(diag)-1
+    i=index-1
+    while i>=0:
+        (row, col)=diag[i]
+        if app.pieces[row][col]!=("empty", "empty"):
+            left=i
+            break
+        i-=1
+    i=index+1
+    while i<len(diag):
+        (row, col)=diag[i]
+        if app.pieces[row][col]!=("empty", "empty"):
+            right=i
+            break
+        i+=1
+    return left, right
+    
+# Returns left diagonal of [currR][currC] as a list
+def Ldiag(currR, currC):
+    result=[]
+    row=currR-1
+    col=currC-1
+    while row>=0 and col>=0:
+        result.append((row, col))
+        row-=1
+        col-=1
+    index=len(result)
+    result.reverse()
+    result.append((currR, currC))
+    row=currR+1
+    col=currC+1
+    while row<8 and col<8:
+        result.append((row, col))
+        row+=1
+        col+=1
+    return result, index
+
+# Returns right diagonal of [currR][currC] as a list
+def Rdiag(currR, currC):
+    result=[]
+    row=currR+1
+    col=currC-1
+    while row<8 and col>=0:
+        result.append((row, col))
+        row+=1
+        col-=1
+    index=len(result)
+    result.reverse()
+    result.append((currR, currC))
+    row=currR-1
+    col=currC+1
+    while row>=0 and col<8:
+        result.append((row, col))
+        row-=1
+        col+=1
+    return result, index
+
+# Returns the row of the closest piece to [currR][currC] on the same column
+def nearestPieceOnRow(app, currR, currC):
+    rightC=7
+    leftC=0
+    i=currC+1
+    while i<8:
+        if app.pieces[currR][i]!=("empty", "empty"):
+            rightC=i
+            break
+        i+=1
+    i=currC-1
+    while i>=0:
+        if app.pieces[currR][i]!=("empty", "empty"):
+            leftC=i
+            break
+        i-=1
+    return leftC, rightC
+
+# Returns the col of the closest piece to [currR][currC] on the same row
+def nearestPieceOnCol(app, currR, currC):
+    topR=7
+    botR=0
+    i=currR+1
+    while i<8:
+        if app.pieces[i][currC]!=("empty", "empty"):
+            topR=i
+            break
+        i+=1
+    i=currR-1
+    while i>=0:
+        if app.pieces[i][currC]!=("empty", "empty"):
+            botR=i
+            break
+        i-=1
+    return topR, botR
 
 # Returns (x, y) center of given cell in grid
 def getCellCenter(app, row, col):
